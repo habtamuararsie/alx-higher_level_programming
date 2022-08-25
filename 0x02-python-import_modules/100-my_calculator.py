01f5cc65d8a27edc18d28606806d7b4dbcf066dc
habtamuararsie <habtamuararsie@yahoo.com>
2022-08-25 13:42:04 +0300

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    len_args = len(argv) - 1
+
+    if len_args == 3:
+        operator = argv[2]
+        num1 = int(argv[1])
+        num2 = int(argv[3])
+        if operator == '+':
+            result = add(num1, num2)
+            print("{:d} + {:d} = {:d}".format(num1, num2, result))
+            exit(0)
+        elif operator == '-':
+            result = sub(num1, num2)
+            print("{:d} - {:d} = {:d}".format(num1, num2, result))
+            exit(0)
+        elif operator == '*':
+            result = mul(num1, num2)
+            print("{:d} * {:d} = {:d}".format(num1, num2, result))
+            exit(0)
+        elif operator == '/':
+            result = div(num1, num2)
+            print("{:d} / {:d} = {:d}".format(num1, num2, result))
+            exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
