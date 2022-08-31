5669aac6443ec70887fb94e7f87b93e48bdb72b2
habtamuararsie <habtamuararsie@yahoo.com>
2022-08-31 10:07:49 +0300

    search replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replac):
+    habte = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            habte.append(replace)
+        else:
+            habte.append(my_list[i])
+            return habte
