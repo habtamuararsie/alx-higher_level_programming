3fac253dbdec981bc715966be07a1d1eb0a67fab
habtamuararsie <habtamuararsie@yahoo.com>
2022-09-01 09:46:29 +0300

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replac):
+def search_replace(my_list, search, replace):
     copy = []
     for i in range(len(my_list)):
         if my_list[i] == search:
