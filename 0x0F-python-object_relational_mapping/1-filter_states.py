98ef645ded858825a5ca1c0db2ff184984b7500e
habtamuararsie <habtamuararsie@yahoo.com>
2022-12-07 10:06:02 +0300

    tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,15 +2,15 @@
 """
 Write a script that lists all states from the database hbtn_0e_0_usa
 """
-
-import MySQLdb
-from sys import argv
-
 if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
     """
+    import MySQLdb
+    from sys import argv
+
+
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
@@ -22,3 +22,5 @@ if __name__ == '__main__':
 
     for row in rows:
         print(row)
+    cur.close()
+    db.close()    
