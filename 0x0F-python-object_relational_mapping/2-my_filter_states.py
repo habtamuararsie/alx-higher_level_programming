ea872748e70b946495fa780a7659a083299e01ad
habtamuararsie <habtamuararsie@yahoo.com>
2022-12-07 10:14:57 +0300

    tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,15 +5,14 @@ displays all values in the states
 where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
 """
-
-import MySQLdb
-from sys import argv
-
 if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
     """
+
+    import MySQLdb
+    from sys import argv
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
@@ -26,3 +25,5 @@ if __name__ == '__main__':
 
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
