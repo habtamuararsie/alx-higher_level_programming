1e13ee7e71feac9c5e08305d29ce3b113a9bb7f3
habtamuararsie <habtamuararsie@yahoo.com>
2022-12-07 10:28:17 +0300

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,19 @@
-    #!/usr/bin/python3
+  #!/usr/bin/python3
 """
-This script lists all states with
-a `name` starting with the letter `N`
-from the database `hbtn_0e_0_usa`.
+Write a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
 """
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the states
-    from the database.
-    """
     import MySQLdb
     from sys import argv
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
-    rows = cur.fetchall()
-    """ cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
-    
-    query_rows = cur.fetchall() 
-    for row in query_rows:"""
-
-    for row in rows:
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
     db.close()
