5a44482d6ac30920f82b09fce820e664d03de55b
habtamuararsie <habtamuararsie@yahoo.com>
2022-12-09 01:28:55 +0300

    tasks update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""Module."""
+"""
+python script that lists all states from the database hbtn_0e_0_usa with
+a given name
+"""
+
 import MySQLdb
 from sys import argv
 
-
-if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("select * from states \
-    where BINARY name = '{}' ORDER BY id ASC;".format(argv[4]))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
     
