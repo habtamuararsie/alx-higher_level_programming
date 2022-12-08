906eea09f8d06cd7a0df6a8be16891c87e5e053d
habtamuararsie <habtamuararsie@yahoo.com>
2022-12-08 22:31:24 +0300

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,17 @@
-  #!/usr/bin/python3
-"""
-Write a script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
-"""
+#!/usr/bin/python3
+"""Module."""
+import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    import MySQLdb
-    from sys import argv
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("select * from states \
+    where BINARY name = '{}' ORDER BY id ASC;".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
