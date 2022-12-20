9d772716b375510c3b9c011c1c0b3cefcbb3c7e3
habtamuararsie <habtamuararsie@yahoo.com>
2022-12-20 11:33:56 +0300

    tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-Usage: ./3-error_code.py <URL>
-  - Handles HTTP errors.
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
 """
+
+
 import sys
 import urllib.error
 import urllib.request
